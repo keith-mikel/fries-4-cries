4fa0f024c56f024c8376273265cc4513e8875bde
keith-mikel <keith.mikel@e5sol.com>
2023-06-08 19:23:28 -0500

    got apis working

--- assets/js/scriptmap.js
+++ assets/js/scriptmap.js
@@ -1,3 +1,8 @@
+var submitButton = document.getElementById('submitButton')
+var userFood = document.getElementById('foodType')
+var foodSuggest = document.getElementById('foodSuggest')
+
+
 var currentLatitude;
 var currentLongitude;
 
@@ -12,45 +17,53 @@ const options = {
   
     console.log("Your current position is:");
     console.log(`Latitude : ${crd.latitude}`);
+    currentLatitude = crd.latitude
     console.log(`Longitude: ${crd.longitude}`);
+    currentLongitude = crd.longitude
     console.log(`More or less ${crd.accuracy} meters.`);
 
-    currentLatitude = crd.latitude
-    currentLongitude = crd.longitude
-
-    var googleAPIKey = 'AIzaSyATKPnuuTLSynb2Cq_YP-ZpPHpxUUyFRiQ'
-
-    // Input variables
-    const latitude = currentLatitude
-    const longitude = currentLongitude;
-    const keywords = 'ice cream';
-
-    // Construct the API request URL
-    const apiKey = googleAPIKey;
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=5000&keyword=${encodeURIComponent(keywords)}&key=${apiKey}`;
-
-    // Make the API request using Fetch
-    fetch(proxyUrl + url)
-    .then(function(response) {
-        if (response.ok) {
-        return response.json();
-        } else {
-        throw new Error('Network response was not ok.');
-        }
-    })
-    .then(function(data) {
-        console.log(data);
-        // Process the retrieved data as needed
-    })
-    .catch(function(error) {
-        console.log('Error:', error.message);
-    });
-
   }
+
+    function getFoodSuggestions() {
+
+      var googleAPIKey = 'AIzaSyATKPnuuTLSynb2Cq_YP-ZpPHpxUUyFRiQ'
+
+      // Input variables
+      const latitude = currentLatitude
+      const longitude = currentLongitude;
+      const keywords = userFood.value;
+
+      // Construct the API request URL
+      const apiKey = googleAPIKey;
+      const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+      const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=5000&keyword=${encodeURIComponent(keywords)}&key=${apiKey}`;
+
+      // Make the API request using Fetch
+      fetch(proxyUrl + url)
+      .then(function(response) {
+          if (response.ok) {
+          return response.json();
+          } else {
+          throw new Error('Network response was not ok.');
+          }
+      })
+      .then(function(data) {
+          console.log(data);
+          foodSuggest.textContent = data.results[0].name + ' Rating: ' + data.results[0].rating
+
+          // Process the retrieved data as needed
+      })
+      .catch(function(error) {
+          console.log('Error:', error.message);
+      });
+
+    }
   
   function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
   }
+
   
   navigator.geolocation.getCurrentPosition(success, error, options);
+
+submitButton.addEventListener('click', getFoodSuggestions)
--- assets/js/scriptsong.js
+++ assets/js/scriptsong.js
@@ -2,6 +2,11 @@
 var clientId = '0e188aa06ff14b0a8bac4604dabcbed8';
 var clientSecret = 'efd2188254884ba4978ddbbdadfc2cb4';
 
+var userGenre = document.getElementById('genre')
+var userSadRate = document.getElementById('sadRate')
+var submitButton = document.getElementById('submitButton')
+var songSuggestion = document.getElementById('songSuggest')
+
 
 // Function to encode client ID and client secret in Base64
 function getEncodedCredentials(clientId, clientSecret) {
@@ -9,45 +14,53 @@ function getEncodedCredentials(clientId, clientSecret) {
   return btoa(credentials);
 }
 
-var spotifyUrl = 'https://api.spotify.com/v1/recommendations?market=ES&seed_genres=country&target_valence=.1';
 
-// Get the access token
-fetch('https://accounts.spotify.com/api/token', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': `Basic ${getEncodedCredentials(clientId, clientSecret)}`
-  },
-  body: 'grant_type=client_credentials'
-})
-.then(function (response) {
-  if (response.ok) {
-    return response.json();
-  } else {
-    throw new Error('Failed to obtain access token');
-  }
-})
-.then(function (data) {
-  var accessToken = data.access_token;
+function getSongSuggestions(spotifyUrl) {
+    // Get the access token
+      fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': `Basic ${getEncodedCredentials(clientId, clientSecret)}`
+        },
+        body: 'grant_type=client_credentials'
+      })
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Failed to obtain access token');
+        }
+      })
+      .then(function (data) {
+        var accessToken = data.access_token;
+        var genre = userGenre.value
+        var valence = userSadRate.value
+        var spotifyUrl = `https://api.spotify.com/v1/recommendations?market=ES&seed_genres=${genre}&target_valence=${valence}`;
+        // Use the access token to make API requests
+        fetch(spotifyUrl, {
+          headers: {
+            'Authorization': `Bearer ${accessToken}`
+          }
+        })
+        .then(function (response) {
+          if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+              console.log(data);
+              songSuggestion.textContent = data.tracks[0].name + ' : ' + data.tracks[0].artists[0].name
 
-  // Use the access token to make API requests
-  fetch(spotifyUrl, {
-    headers: {
-      'Authorization': `Bearer ${accessToken}`
-    }
-  })
-  .then(function (response) {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        console.log(data);
+            });
+          } else {
+            throw new Error('API request failed');
+          }
+        });
+      })
+      .catch(function (error) {
+        console.error(error);
       });
-    } else {
-      throw new Error('API request failed');
-    }
-  });
-})
-.catch(function (error) {
-  console.error(error);
-});
+}
+
+submitButton.addEventListener('click' , getSongSuggestions)
+
 
