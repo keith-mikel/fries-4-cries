6f5f88b5b9a535a2cbe6417f8f5c0a6e50343170
keith-mikel <keith.mikel@e5sol.com>
2023-06-15 18:59:45 -0500

    added alret for proxy

--- assets/js/scriptmap.js
+++ assets/js/scriptmap.js
@@ -101,6 +101,10 @@ const options = {
 
 submitButton.addEventListener('click', getFoodSuggestions)
 
+if (window.confirm('Click OK to be redirected to activate proxy')) {
+  window.open('https://cors-anywhere.herokuapp.com/corsdemo', '_blank');
+}
+
  
   
 
