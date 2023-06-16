9076c608842dea54b5a5ca03cda2615464e294c3
keith-mikel <keith.mikel@e5sol.com>
2023-06-15 20:25:56 -0500

    change to line up results

--- assets/js/scriptstorage.js
+++ assets/js/scriptstorage.js
@@ -1,97 +1,84 @@
 var resultNum = 1;
 
 function loadHistory() {
-    // Load song history
-    var songHistory = JSON.parse(localStorage.getItem('songHistory')) || [];
-    var songHistoryContainer = document.getElementById('songHistoryContainer');
+  // Load song history
+  var songHistory = JSON.parse(localStorage.getItem('songHistory')) || [];
+  var songHistoryContainer = document.getElementById('songHistoryContainer');
 
-    // Create elements for each song history item
-    songHistory.forEach(function (item) {
-      var songName = item.name;
-      var songUrl = item.url;
+  // Create elements for each song history item
+  songHistory.forEach(function (item, index) {
+    var songName = item.name;
+    var songUrl = item.url;
 
-          //create a title and append it
-          var resultTitle = document.createElement('h1');
-          resultTitle.classList.add('result-title');
-          resultTitle.textContent = "--------";
-          songHistoryContainer.appendChild(resultTitle);
+    // Create a title and append it
+    var resultTitle = document.createElement('h1');
+    resultTitle.classList.add('result-title');
+    resultTitle.textContent = "--------";
+    songHistoryContainer.appendChild(resultTitle);
 
-      //create the boxes around the item
-      var titleBar = document.createElement('div');
-      titleBar.classList.add('aesthetic-windows-95-modal-title-bar');
-      var titleBarText = document.createElement('div');
-      titleBarText.classList.add('aesthetic-windows-95-modal-title-bar-text');
-      titleBarText.textContent = "song.txt";
-      var songContent = document.createElement('div');
-      songContent.classList.add('aesthetic-windows-95-modal-content');
-  
-      // Create text element for song name
-      var songNameElement = document.createElement('p');
-      songNameElement.textContent = songName;
-  
-      // Create iframe element for song URL
-      var songImgElement = document.createElement('img');
-      songImgElement.src = songUrl;
 
-      //append everything in order
-      songHistoryContainer.appendChild(titleBar);
-      titleBar.appendChild(titleBarText);
-      songHistoryContainer.appendChild(songContent);
-      songContent.appendChild(songNameElement);
-      songContent.appendChild(songImgElement);
-    });
-  
-    // Load map history
-    var mapHistory = JSON.parse(localStorage.getItem('mapHistory')) || [];
-    var mapHistoryContainer = document.getElementById('mapHistoryContainer');
-  
-    // Create elements for each map history item
-    mapHistory.forEach(function (item) {
-      var mapName = item.name;
-      var mapUrl = item.url;
+    // Create the boxes around the item
+    var songContainer = document.createElement('div');
+    songContainer.classList.add('aesthetic-windows-95-modal');
+    var songContent = document.createElement('div');
+    songContent.classList.add('aesthetic-windows-95-modal-content');
 
-        
-          //create a title and append it
-          var resultTitle = document.createElement('h1');
-          resultTitle.classList.add('result-title');
-          resultTitle.textContent = "Result " + resultNum;
-          newResultNum();
-          mapHistoryContainer.appendChild(resultTitle);
+    // Create text element for song name
+    var songNameElement = document.createElement('p');
+    songNameElement.textContent = songName;
 
-      //create the boxes around the item
-      var titleBar = document.createElement('div');
-      titleBar.classList.add('aesthetic-windows-95-modal-title-bar');
-      var titleBarText = document.createElement('div');
-      titleBarText.classList.add('aesthetic-windows-95-modal-title-bar-text');
-      titleBarText.textContent = "food.txt";
-      var mapContent = document.createElement('div');
-      mapContent.classList.add('aesthetic-windows-95-modal-content');
-  
-      // Create text element for map name
-      var mapNameElement = document.createElement('p');
-      mapNameElement.textContent = mapName;
-      mapHistoryContainer.appendChild(mapNameElement);
-  
-      // Create img element for map URL
-      var mapImgElement = document.createElement('img');
-      mapImgElement.src = mapUrl;
-      mapHistoryContainer.appendChild(mapImgElement);
+    // Create img element for song URL
+    var songImgElement = document.createElement('img');
+    songImgElement.src = songUrl;
 
-            //append everything in order
-            mapHistoryContainer.appendChild(titleBar);
-            titleBar.appendChild(titleBarText);
-            mapHistoryContainer.appendChild(mapContent);
+    // Append everything in order
+    songContainer.appendChild(songContent);
+    songContent.appendChild(songNameElement);
+    songContent.appendChild(songImgElement);
+    songHistoryContainer.appendChild(songContainer);
+  });
 
-            mapContent.appendChild(mapNameElement);
-            mapContent.appendChild(mapImgElement);
-    });
-  }
+  // Load map history
+  var mapHistory = JSON.parse(localStorage.getItem('mapHistory')) || [];
+  var mapHistoryContainer = document.getElementById('mapHistoryContainer');
 
-  window.addEventListener('load', loadHistory);
+  // Create elements for each map history item
+  mapHistory.forEach(function (item) {
+    var mapName = item.name;
+    var mapUrl = item.url;
 
-  //function for the number to increase properly
-  function newResultNum() {
-    resultNum = resultNum + 1;
-  }
+    // Create a title and append it
+    var resultTitle = document.createElement('h1');
+    resultTitle.classList.add('result-title');
+    resultTitle.textContent = "Result " + resultNum;
+    newResultNum();
+    mapHistoryContainer.appendChild(resultTitle);
 
-  
+    // Create the boxes around the item
+    var mapContainer = document.createElement('div');
+    mapContainer.classList.add('aesthetic-windows-95-modal');
+    var mapContent = document.createElement('div');
+    mapContent.classList.add('aesthetic-windows-95-modal-content');
+
+    // Create text element for map name
+    var mapNameElement = document.createElement('p');
+    mapNameElement.textContent = mapName;
+
+    // Create img element for map URL
+    var mapImgElement = document.createElement('img');
+    mapImgElement.src = mapUrl;
+
+    // Append everything in order
+    mapContainer.appendChild(mapContent);
+    mapContent.appendChild(mapNameElement);
+    mapContent.appendChild(mapImgElement);
+    mapHistoryContainer.appendChild(mapContainer);
+  });
+}
+
+window.addEventListener('load', loadHistory);
+
+// Function for the number to increase properly
+function newResultNum() {
+  resultNum = resultNum + 1;
+}
